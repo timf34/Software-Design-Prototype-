a47d33b777a5461ffb2894fd8ee502a0e9540603
szkaradj <szkaradj@tcd.ie>
2022-02-21 01:47:10 +0000

    Added the bottom tab navigator

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Login from './screens/Login'
 import Home from './screens/Home'
@@ -9,12 +10,39 @@ import User from './screens/User'
 import Settings from './screens/Settings';
 import Product from './screens/Product';
 
+const BottomTab = () => {
+
+  const Tab = createBottomTabNavigator();
+
+  return(
+    <Tab.Navigator>
+      <Tab.Screen 
+        name = 'Home'
+        component={Home}
+      />
+      <Tab.Screen 
+        name = 'User'
+        component={User}
+      />
+      <Tab.Screen 
+        name = 'Settings'
+        component={Settings}
+      />
+    </Tab.Navigator>
+  );
+
+}
+
 export default Routes = () => {
     const Stack = createNativeStackNavigator()
 
     return (
       <NavigationContainer>
         <Stack.Navigator>
+          <Stack.Screen
+            name = 'BottomTab'
+            component={BottomTab}
+          />
           <Stack.Screen
             name='Login'
             component={Login}
