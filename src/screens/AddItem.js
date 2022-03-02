319e81b3e96a3d99042e95a57236a7600ed78e63
szkaradj <szkaradj@tcd.ie>
2022-03-02 15:30:44 +0000

    Added basic image upload for additem page

--- src/screens/AddItem.js
+++ src/screens/AddItem.js
@@ -1,26 +1,119 @@
-import {ref, getDownloadURL } from 'firebase/storage';
-import React, { useState } from 'react'
-import { Image, Text, SafeAreaView, View,StyleSheet} from 'react-native';
+import {ref, getDownloadURL, uploadBytes } from 'firebase/storage';
+import React, { useState, useEffect } from 'react'
+import { Button, Image, Text, SafeAreaView, View,TextInput,StyleSheet} from 'react-native';
 import { storage } from '../../firebase';
+import * as ImagePicker from 'expo-image-picker';
+import {doc, setDoc, getDoc} from 'firebase/firestore';
+import { nanoid } from 'nanoid';
+import { useNavigation } from '@react-navigation/native';
 
+async function uploadImage(uri, fname) {
 
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        console.log(e);
+        reject(new TypeError("Network request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+    });
+  
+    const fileName = fname;
+    const imageRef = ref(storage, `${fileName}.jpeg`);
+  
+    const snapshot = await uploadBytes(imageRef, blob, {
+      contentType: "image/jpeg",
+    });
+  
+    blob.close();
+  
+    return {fileName};
+  }
 
-
-export default function AddItem(){
+export default function AddItem({navigation}){
     //handle image url request from firestore
-    const[image, setImage] = useState();
-    imageRef = ref(storage, 'Screenshot_2022-01-26-08-50-53-28_1c337646f29875672b5a61192b9010f9.jpg');
+    const[image, setImage] = useState('default.jpeg');
+    const[url, setUrl] = useState();
+    const[name, setName] = useState();
+    const[desc, setDesc] = useState();
+    const[itemType, setItemType] = useState();
 
-    //if image is null, get url from the database
-    if(!image){
-        getDownloadURL(imageRef)
-        .then((url)=>{setImage(url);});
-    }
+    const getImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+
+        if (!result.cancelled) {
+            const filename = uploadImage(result.uri, name);
+            console.log(filename);
+            getDownloadURL(ref(storage, `${filename}.jpeg`))
+            .then((url)=>{setUrl(url)});
+        };
     
+    }
+
+    // const pickImage = async() => {
+    //     const result = getImage();
+    //     filename = await uploadImage(result.uri);
+    //     setImage(filename)
+    // }
+
+    const updateDatabase = async() => {
+        await setDoc((db, "Items", image), {
+            name: name,
+            type: itemType,
+            desc: desc,
+            imageUrl: image
+        });
+
+        navigation.navigate('Product');
+    }
+
+    getDownloadURL(ref(storage, image))
+    .then((url)=>{setUrl(url)});
+
     return(
-        <SafeAreaView>
+        <SafeAreaView style={styles.container}>
             <View style={styles.imageBox}>
-                <Image source={{uri: image}} style={styles.image}/>
+                <Image source={{uri: url}} style={styles.image}/>
+            
+            <Button
+                titleStyle={{fontSize: 30,}}
+                onPress={getImage}
+                title = "choose image"
+            />
+            <TextInput
+                style={styles.textBox}
+                onChangeText={setName}
+                value={name}
+                placeholder ='Item Name'
+            />
+            <TextInput
+                style={styles.textBox}
+                onChangeText={setItemType}
+                value={itemType}
+                placeholder ='Item Type'
+            />
+            <TextInput
+                style={styles.textBox}
+                onChangeText={setDesc}
+                value={desc}
+                placeholder ='Item Description'
+            />
+            <Button
+                style={styles.button}
+                onPress={updateDatabase}
+                title = "Add item"
+            />
             </View>
         </SafeAreaView>
     );
@@ -36,9 +129,19 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 
+    textBox:{
+        fontSize: 30,
+    },
+
     image:{
         width: '100%',
-        height: '100%',
+        height: '70%',
     },
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+
+    },   
 
 })
--- src/screens/Product.js
+++ src/screens/Product.js
@@ -1,75 +1,49 @@
-import React, { useState } from 'react'
-import { ImageBackground,Dimensions, StyleSheet,View, Text } from 'react-native';
 import {ref, getDownloadURL } from 'firebase/storage';
-import {storage} from '../../firebase';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useState, useEffect } from 'react'
+import { Image, Text, SafeAreaView, View,StyleSheet} from 'react-native';
+import { storage, db } from '../../firebase';
 
-export default function Product({navigation, route}){
 
+
+
+
+export default function Product(props){
+    //handle image url request from firestore
     const[image, setImage] = useState();
-    imageRef = ref(storage, 'Screenshot_2022-01-26-08-50-53-28_1c337646f29875672b5a61192b9010f9.jpg');
+    imageRef = ref(storage, 'sketch.jpg');
 
-    if(!image){
+
+
+        //if image is null, get url from the database
+        if(!image){
         getDownloadURL(imageRef)
         .then((url)=>{setImage(url);});
-    }
-
-    return (
-
+        }
+  
+    
+    
+    return(
         <SafeAreaView>
-            <View style={styles.container}>
-                <View style={styles.picture}>
-                    <ImageBackground source={{uri: image}} style={{width: Dimensions.get('window').width, height: Dimensions.get('window').width}}></ImageBackground>
-                        <View style={styles.infoBox}>
-                            <View style={styles.title}>
-                                <Text style={styles.textTitle}> Title</Text>
-                            </View>
-                            <View style={styles.description}>
-                            <Text style={styles.textDescription}>Description Description Description Description Description Description Description Description Description Description Description Description </Text>
-                            </View>
-                    </View>
-                </View>
+            <View style={styles.imageBox}>
+                <Image source={{uri: image}} style={styles.image}/>
             </View>
         </SafeAreaView>
-        
-
     );
+
+
 }
 
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent:'center',
-        backgroundColor: '#012E40'
+
+    imageBox: {
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-    picture: {
-        backgroundColor: '#fff',
-    },
-    infoBox: {
-        backgroundColor: '#012E40',
-        alignSelf: 'flex-end',
-        height: Dimensions.get('window').height - Dimensions.get('window').width,
+
+    image:{
         width: '100%',
+        height: '100%',
     },
-    title:{
-        flex: 1,
-        backgroundColor: '#025159',
-    },
-    textTitle:{
-        color: '#F28705',
-        alignSelf: 'auto',
-        margin: 5,
-        fontSize: 40,
-    },
-    description:{
-        flex: 3,
-        backgroundColor: '#012E40',
-    },
-    textDescription:{
-        color: '#03A696',
-        alignSelf: 'auto',
-        margin: 5,
-        fontSize: 20,
-    },
-  
-  });
+
+})
