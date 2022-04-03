32b71f34b8a96c340ba964134ad8f861ecd7e448
szkaradj <szkaradj@tcd.ie>
2022-04-03 14:47:55 +0100

    Improved load times through image compression
    Changed the image picker settings to compress the image before uploading. It should make the load times on explore page much quicker.

--- src/screens/AddItem.js
+++ src/screens/AddItem.js
@@ -66,7 +66,7 @@ export default function AddItem({navigation}){
           mediaTypes: ImagePicker.MediaTypeOptions.All,
           allowsEditing: true,
           aspect: [4, 3],
-          quality: 1,
+          quality: 0.5,
         });
 
         console.log(result);
